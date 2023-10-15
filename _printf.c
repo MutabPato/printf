1403913496b2d12b510e9eb41ecb8e478970a22b
MutabPato <patrickmtbz@gmail.com>
2023-10-15 10:51:51 -0700

    handling specifiers

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
+int _strlen(char *str);
 
 /**
  * _printf - produces output according to a format
@@ -11,13 +11,50 @@
 
 int _printf(const char *format, ...)
 {
-  va_list args;
-  char *str;
-  int len;
-  
-  va_start(args, format);
-  str = va_arg(args, char *);
-  len = strlen(str);
-  va_end(args);
-  return (len);
+	va_list args;
+	char *str;
+	int len, j;
+
+	j = 0;
+	while (*format)
+	{
+		if (format[j] == '%')
+		{
+			switch (format[j+1])
+			{
+				case 'c':
+					va_arg(args, int);
+					break;
+				case 's':
+					va_arg(args, char *);
+					break;
+				default:
+					break;
+			}
+			j++;
+		}
+	}
+	va_start(args, format);
+	str = va_arg(args, char *);
+	len = _strlen(str);
+	va_end(args);
+	return (len);
+}
+
+/**
+ * _strlen - finds string length
+ * @str: pointer to string
+ *
+ * Return: string length.
+ */
+
+int _strlen(char *str)
+{
+	int i;
+	int length = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
+	return (length);
 }
