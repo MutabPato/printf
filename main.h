c2b270581beefaf843ff1ed61db76179cb5e91ee
ShalyneKerubo <shalyne.kerubo@gmail.com>
2023-10-17 09:49:15 -0700

    Printf Project

--- main.h
+++ main.h
@@ -2,6 +2,12 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
+int process_format_specifier(const char **format, va_list args)
+int print_char(va_list args);
+int print_string(va_list args);
+int print_integer(va_list args);
+int print_percent(void);
+
 int _strlen(char *str);
 int _itoa(int num);
 
