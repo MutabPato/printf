c7f94b0b4366a1ba9704adcdde02b7f8368ce44d
MutabPato <patrickmtbz@gmail.com>
2023-10-17 11:13:23 -0700

    Handling non-specifiers

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to a format.
+ * @format: a character string
+ *
+ * Return: no. of characters printed (excluding the null byte).
+ */
+int _printf(const char *format, ...)
+{
+        va_list args;
+        int count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+        va_start(args, format);
+        while (*format)
+        {
+                if (*format == '%')
+                {
+                        format++;
+                        count += process_format_specifier(&format, args);
+                }
+                else
+                {
+                        write(1, format, 1);
+                        count++;
+                }
+                format++;
+        }
+        va_end(args);
+        return (count);
+}
+
--- main.c
+++ main.c
@@ -28,8 +28,8 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 
-/*	_printf("%\n"); 
-	printf( "%\n"); */
+	_printf("1percentage %t \n"); 
+	printf("2percentage %t \n");
 	
 	return (0);
 }
