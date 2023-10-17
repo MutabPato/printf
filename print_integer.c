bfe948f5d62a28ed7377ab1145223b49f691eac6
MutabPato <patrickmtbz@gmail.com>
2023-10-17 09:41:56 -0700

    breaking down

---
+++ print_integer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * print_integer - replace format specifier with integer
+ * @args : arguments passed to the function
+ *
+ * Return : length of output.
+ */
+int print_integer(va_list args)
+{
+	int num = va_args(args, int);
+	int len = _itoa(num);
+	return (len);
+}
---
+++ print_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * print_str - replace format specifier with string
+ * @args : arguments passed to the function
+ *
+ * Return : character to be printed.
+ */
+int print_string(va_list args)
+{
+	char *str = va_args(args, char *);
+	int len = _strlen(str);
+	return (write(1, str, len));
+}
